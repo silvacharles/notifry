930b7a6e5e0f02f533ec945b960ad2520869b115
freefoote <freefoote@ce674eb3-4e32-b282-9e7e-a1069e2c5fe8>
2011-03-05 05:56:38 +0000

    Very, very basic App Engine structure using web.py.

---
+++ appengine/compile-templates.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python web/template.py --compile templates
---
+++ appengine/index.py
@@ -0,0 +1,12 @@
+import web
+
+urls = ('/', 'index')
+render = web.template.render('templates/')
+
+class index:
+	def GET(self):
+		return render.index()
+
+app = web.application(urls, globals())
+main = app.cgirun()
+
