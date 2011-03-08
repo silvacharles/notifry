4f2bd4e398da1ae723c662b8be0cbf7eabea51b6
freefoote <freefoote@ce674eb3-4e32-b282-9e7e-a1069e2c5fe8>
2011-03-08 11:56:06 +0000

    Added login/logout. Still figuring out web.py.

--- appengine/index.py
+++ appengine/index.py
@@ -1,11 +1,32 @@
 import web
+from google.appengine.api import users
 
-urls = ('/', 'index')
+urls = (
+	'/', 'index',
+	'/login', 'login',
+	'/logout', 'logout'
+)
+
 render = web.template.render('templates/')
 
 class index:
 	def GET(self):
-		return render.index()
+		return render.layout(render.index(), users.get_current_user())
+
+class login:
+	def GET(self):
+		user = users.get_current_user()
+
+		if user:
+			# Is logged in.
+			raise web.seeother('/')
+		else:
+			# Not logged in - redirect to login.
+			raise web.seeother(users.create_login_url(web.url()))
+
+class logout:
+	def GET(self):
+		raise web.seeother(users.create_logout_url("/"))
 
 app = web.application(urls, globals())
 main = app.cgirun()
